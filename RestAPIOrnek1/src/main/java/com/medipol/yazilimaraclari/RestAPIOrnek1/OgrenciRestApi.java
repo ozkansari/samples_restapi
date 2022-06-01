b7ff55691d30c6bc320524cd6064e9d8e1a0ec7c
ozkansari <ozkansari@gmail.com>
2022-06-01 19:11:06 +0200

    Rest api ornek

---
+++ RestAPIOrnek1/src/main/java/com/medipol/yazilimaraclari/RestAPIOrnek1/OgrenciRestApi.java
@@ -0,0 +1,55 @@
+package com.medipol.yazilimaraclari.RestAPIOrnek1;
+
+import java.util.*;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/ogrenci")
+public class OgrenciRestApi {
+
+	private static List<Ogrenci> OGRENCILER = new ArrayList<>();
+	
+	@GetMapping("listele")
+	public List<Ogrenci> listele() {
+		return OGRENCILER;
+	}
+	
+	@PostMapping("/ekle")
+	public String ekle(@RequestBody Ogrenci ogrenci) {
+		OGRENCILER.add(ogrenci);
+		return ogrenci.numara;
+	}
+	
+	public record Ogrenci(String isim, String numara) {
+		
+	}
+
+	/*--
+	public static class Ogrenci {
+		private String isim;
+		private String numara;
+		
+		public Ogrenci(String isim, String numara) {
+			super();
+			this.isim = isim;
+			this.numara = numara;
+		}
+		
+		public String getIsim() {
+			return isim;
+		}
+		public void setIsim(String isim) {
+			this.isim = isim;
+		}
+		
+		public String getNumara() {
+			return numara;
+		}
+		public void setNumara(String numara) {
+			this.numara = numara;
+		}
+	}
+	*/
+	
+	
+}
